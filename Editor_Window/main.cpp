5511e807468608207e323f6eef1a113a8757b402
원표 조 <whdnjsvy@snu.ac.kr>
2026-01-22 11:17:16 +0900

    정적 라이브러리 생성 및 응용 프로젝트에 연결

--- Editor_Window/main.cpp
+++ Editor_Window/main.cpp
@@ -3,6 +3,7 @@
 
 #include "framework.h"
 #include "Editor_Window.h"
+#include "..\\YamYamEngine_SOURCE\\waApplication.h"
 
 #define MAX_LOADSTRING 100
 
@@ -17,10 +18,11 @@ BOOL                InitInstance(HINSTANCE, int);
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
 
-int APIENTRY wWinMain(_In_ HINSTANCE hInstance,     // 프로그램의 인스턴스 핸들
-                     _In_opt_ HINSTANCE hPrevInstance,
-                     _In_ LPWSTR    lpCmdLine,
-                     _In_ int       nCmdShow)
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,         // 프로그램의 인스턴스 핸들
+                     _In_opt_ HINSTANCE hPrevInstance,  // 바로 앞에 실행된 현재 프로그램의 인스턴스 핸들, 없을 경우에는 NULL
+                                                        // 지금은 신경쓰지 않아도 되는 값이다.
+                     _In_ LPWSTR    lpCmdLine,          // 명령행 으로 입력된 프로그램 인수
+                     _In_ int       nCmdShow)           // 프로그램이 실행될 형태이며, 보통 모양 정보등이 전달된다.
 {
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
@@ -43,6 +45,34 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,     // 프로그램의 인스턴
     HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_EDITORWINDOW));
 
     MSG msg;
+
+    // GetMessage(&msg, nullptr, 0, 0)
+    // 프로세스에서 발생한 메세지를 메세지 큐에서 가져오는 함수
+    // 메세지큐에 아무것도 없다면?? -> 아무 메세지도 가져오지 않게 된다.
+
+    // PeekMessage : 메세지 큐에 메세지 유무에 상관없이 함수가 리턴된다.
+    //                  리턴 값이 true 인 경우 메세지가 있고 false 인 경우는 메세지가 없다라고 가르쳐 준다.
+
+    while (true)
+    {
+        if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+        {
+            if (msg.message == WM_QUIT)
+                break;
+
+            if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+            {
+                TranslateMessage(&msg);
+                DispatchMessage(&msg);
+            }
+        }
+        else
+        {
+            int a = 0;
+            // 메세지가 없을 경우 여기서 처리
+            // 게임 로직이 들어가면 된다.
+        }
+    }
 
     // 기본 메시지 루프입니다:
     while (GetMessage(&msg, nullptr, 0, 0))
@@ -148,6 +178,42 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         {
             PAINTSTRUCT ps;
             HDC hdc = BeginPaint(hWnd, &ps);
+            
+            // 파랑 브러쉬 생성
+            HBRUSH brush = CreateSolidBrush(RGB(0, 0, 255));
+            
+            // 파랑 브러쉬 DC에 선택 그리고 흰색 브러쉬 반환
+            HBRUSH oldBrush = (HBRUSH)SelectObject(hdc, brush);
+
+            Rectangle(hdc, 100, 100, 200, 200);
+
+            // 다시 흰색 원본 브러쉬로 선택
+            SelectObject(hdc, oldBrush);
+
+            // 파랑 브러쉬 삭제
+            DeleteObject(brush);
+
+            HPEN redPen = CreatePen(PS_SOLID, 2, RGB(255, 0, 0));
+            HPEN oldPen = (HPEN)SelectObject(hdc, redPen);
+
+            Ellipse(hdc, 200, 200, 300, 300);
+
+            SelectObject(hdc, oldPen);
+            DeleteObject(redPen);
+
+            // 기본으로 자주 사용되는 GDI 오브젝트를 미리 DC 안에 만들어 두었는데
+            // 그 오브젝트들을 스톡 오브젝트라고 한다.
+
+            HBRUSH grayBrush = (HBRUSH)GetStockObject(GRAY_BRUSH);
+            oldBrush = (HBRUSH)SelectObject(hdc, grayBrush);
+            Rectangle(hdc, 400, 400, 500, 500);
+            SelectObject(hdc, oldBrush);
+
+            // DC란 화면에 출력에 필요한 모든 정보를 가지는 데이터 구조체이며
+            // GDI 모듈에 의해서 관리된다.
+            // 어떤 폰트를 사용할 것인가? 어떤 선의 굵기를 정해줄 것인지? 어떤 색상으로 그려줄 것인지?
+            // 화면 출력에 필요한 모든 경우는 WINAPI에서는 DC를 통해서 작업을 진행할 수 있다.
+
             // TODO: 여기에 hdc를 사용하는 그리기 코드를 추가합니다...
             EndPaint(hWnd, &ps);
         }
---
+++ YamYamEngine_SOURCE/waApplication.h
@@ -0,0 +1,11 @@
+#pragma once
+class Application
+{
+public:
+	void test()
+	{
+		int a = 0;
+	}
+
+};
+
